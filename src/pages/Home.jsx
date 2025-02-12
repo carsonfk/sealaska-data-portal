46ceeb77df6e9f73e0b880a6b5c82ca487704816
carsonfk <carson.klein@sealaska.com>
2025-02-11 16:56:16 -0900

    update home.jsx with map mode

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,18 @@ import Map from "../components/Map";
 import FilterForm from "../components/FilterForm";
 import {getDatabase, ref, onValue, get, orderByChild, equalTo, query } from 'firebase/database'
 import Hero from "../components/Hero";
+import ViewContributeForm from "../components/ViewContributeForm";
 
 export default function Home(props){
 	const [data, setData] = useState()
 	//const [filter, setFilter] = useState('null')
 	const [reset, setReset] = useState(0);
 	const mapRef = useRef(null);
-    const [statusVC, setStatusVC] = useState("v");
+    const [mapMode, setMapMode] = useState("v");
 
-
+    const handleFormSubmit = (selectedMode) => { //this has to do with updating filter value when the filter form is submitted
+		setMapMode(selectedMode);
+	};
 
 
 
@@ -28,6 +31,7 @@ export default function Home(props){
         <main>
             
 			<div className="content">
+                <ViewContributeForm onSubmit={handleFormSubmit} />
 				<Map locations={data}/>
 				<AddFeatureForm />
 			</div>
