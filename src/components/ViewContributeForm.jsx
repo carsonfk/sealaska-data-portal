729b4d51164bcab6b11bb0679bf4cd00cbb52c15
carsonfk <carson.klein@sealaska.com>
2025-02-11 16:42:43 -0900

    update map mode form

---
+++ src/components/ViewContributeForm.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function ViewContributeForm( {onSubmit} ) {
+    //returns the value that is filled in the form to Home.
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const selectedMode = event.target.elements['map-mode'].value;
+        onSubmit(selectedMode);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="select-type">
+                <label htmlFor="map-mode"><b>Map mode: </b></label>
+                <label htmlFor="view">View</label>
+                <input type="radio" id="view" name="map-mode" value="view"/>
+                <label htmlFor="contribute">Contribute</label>
+                <input type="radio" id="contribute" name="map-mode" value="contribute"/>
+                <input type="submit" value="Swap mode"/>
+            </div>
+      </form>
+    )
+}
