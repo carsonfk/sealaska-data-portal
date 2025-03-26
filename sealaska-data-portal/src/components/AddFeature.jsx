3f101fd1d064d23c037d43d0ba08967d905cf918
carsonfk <carson.klein@sealaska.com>
2025-03-25 16:27:01 -0800

    new location form appears depending on mode

--- sealaska-data-portal/src/components/AddFeature.jsx
+++ sealaska-data-portal/src/components/AddFeature.jsx
@@ -44,67 +44,69 @@ export default function AddFeatureForm( {mode, selectionCoordinates, onEdit} ) {
     }
 
     //add style to "sharing-buttons": style={{ border: "2px solid black", borderRadius: "6px", padding: "10px", display: "flex", alignItems: "center", justifyContent: "center", background: "white" }}
-    return (
-        <div className="add-location-form">
-            <h2>Add New Location</h2>
-            <form id="newLocationForm" onSubmit={onSubmit}>
-                <label htmlFor="latitude">Latitude:</label>
-                <input
-                    type="number"
-                    min="54.5"
-                    max="60"
-                    step="0.5"
-                    id="latitude"
-                    name="latitude"
-                    value={form.latitude}
-                    onChange={(e) => {
-                        onEdit([e.target.value, form.longitude]);
-                        updateForm({ latitude: e.target.value });
-                    }}
-                />
+    if (mode === 'contribute') {
+        return (
+            <div className="add-location-form">
+                <h2>Add New Location</h2>
+                <form id="newLocationForm" onSubmit={onSubmit}>
+                    <label htmlFor="latitude">Latitude:</label>
+                    <input
+                        type="number"
+                        min="54.5"
+                        max="60"
+                        step="0.25"
+                        id="latitude"
+                        name="latitude"
+                        value={form.latitude}
+                        onChange={(e) => {
+                            onEdit([e.target.value, form.longitude]);
+                            updateForm({ latitude: e.target.value });
+                        }}
+                    />
 
-                <label htmlFor="longitude">Longitude:</label>
-                <input
-                    type="number"
-                    min="-140"
-                    max="-130"
-                    step="0.5"
-                    id="longitude"
-                    name="longitude"
-                    value={form.longitude}
-                    onChange={(e) => {
-                        onEdit([form.latitude, e.target.value]);
-                        updateForm({ longitude: e.target.value });
-                    }}
-                />
-                <label htmlFor="type">Type:</label>
-                <input
-                    type="text"
-                    id="type"
-                    name="type"
-                    value={form.type}
-                    onChange={(e) => updateForm({ type: e.target.value }, '')}
-                />
+                    <label htmlFor="longitude">Longitude:</label>
+                    <input
+                        type="number"
+                        min="-140"
+                        max="-130"
+                        step="0.25"
+                        id="longitude"
+                        name="longitude"
+                        value={form.longitude}
+                        onChange={(e) => {
+                            onEdit([form.latitude, e.target.value]);
+                            updateForm({ longitude: e.target.value });
+                        }}
+                    />
+                    <label htmlFor="type">Type:</label>
+                    <input
+                        type="text"
+                        id="type"
+                        name="type"
+                        value={form.type}
+                        onChange={(e) => updateForm({ type: e.target.value }, '')}
+                    />
 
-                <label htmlFor="details">Details:</label>
-                <input
-                    type="text"
-                    id="details"
-                    name="details"
-                    value={form.details}
-                    onChange={(e) => updateForm({ details: e.target.value }, '')}
-                />
+                    <label htmlFor="details">Details:</label>
+                    <input
+                        type="text"
+                        id="details"
+                        name="details"
+                        value={form.details}
+                        onChange={(e) => updateForm({ details: e.target.value }, '')}
+                    />
 
-                <div className="sharing-buttons" >
-                    <label htmlFor="sharing"><b>Sharing:</b></label>
-                    <label htmlFor="uncovered">Share to public: </label>
-                    <input style={{width: '20px', height: '20px', marginRight: '10px' }} type="radio" id="public" name="sharing-type" value="public" defaultChecked onChange={(e) => updateForm({ sharing: "public" })}/>
-                    <label htmlFor="public">For Sealaska use only: </label>
-                    <input style={{width: '20px', height: '20px', marginRight: '10px' }} type="radio" id="sealaska" name="sharing-type" value="sealaska" onChange={(e) => updateForm({ sharing: "sealaska" })}/>
-                </div>
+                    <div className="sharing-buttons" >
+                        <label htmlFor="sharing"><b>Sharing:</b></label>
+                        <label htmlFor="uncovered">Share to public: </label>
+                        <input style={{width: '20px', height: '20px', marginRight: '10px' }} type="radio" id="public" name="sharing-type" value="public" defaultChecked onChange={(e) => updateForm({ sharing: "public" })}/>
+                        <label htmlFor="public">For Sealaska use only: </label>
+                        <input style={{width: '20px', height: '20px', marginRight: '10px' }} type="radio" id="sealaska" name="sharing-type" value="sealaska" onChange={(e) => updateForm({ sharing: "sealaska" })}/>
+                    </div>
 
-                <button className="addLocation" type="submit">Submit your Location</button>
-            </form>
-        </div>
-    )
+                    <button className="addLocation" type="submit">Submit your Location</button>
+                </form>
+            </div>
+        )
+    }
 }
