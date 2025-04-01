02526b3cd4d76880f46e4505622c6b3f7bde6081
carsonfk <carson.klein@sealaska.com>
2025-04-01 12:47:02 -0800

    minor error fixed

--- sealaska-data-portal/src/components/ListFeatures.jsx
+++ sealaska-data-portal/src/components/ListFeatures.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 //import { getDatabase, ref, set as firebaseSet, push } from 'firebase/database'
 //import FilterForm from './FilterForm'
 
-export default function ListFeatures() {
+export default function ListFeatures( {mode} ) {
+    
+
     
     if (mode === 'view') {
         return (
