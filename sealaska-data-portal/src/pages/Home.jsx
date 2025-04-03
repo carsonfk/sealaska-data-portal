237c60ea47499de89ae134a214f03b804880bd25
carsonfk <carson.klein@sealaska.com>
2025-04-02 16:47:48 -0800

    style and structure edits

--- sealaska-data-portal/src/pages/Home.jsx
+++ sealaska-data-portal/src/pages/Home.jsx
@@ -79,16 +79,17 @@ export default function Home(props){
         <main>
             
 			<div className="content">
-				<div className = "list">
+				<div className = "contribute">
 					<Hero scrollToMap={scrollToMap}/>
+					<ViewContributeForm onSubmit={handleFormSubmit} />
+					<AddFeatureForm mode={mapMode} selectionCoordinates={currentSelection} onEdit={handleEdits}/>
+					<ListFeatures mode={mapMode}/>
 				</div>
 				<div className="map">
 					<Map locations={data} mode={mapMode} reset={reset} selectionCoordinates={currentSelection} onSelect={handleCurrentSelection}/>
 				</div>
-				<div className="contribute">
-					<ViewContributeForm onSubmit={handleFormSubmit} />
-					<AddFeatureForm mode={mapMode} selectionCoordinates={currentSelection} onEdit={handleEdits}/>
-					<ListFeatures mode={mapMode}/>
+				<div className="options">
+					
 				</div>
 			</div>
 
