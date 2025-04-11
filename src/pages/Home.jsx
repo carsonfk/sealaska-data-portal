a8ab4bff06aed06ee590e316aa381a7b6caa3c76
carsonfk <carson.klein@sealaska.com>
2025-04-11 15:14:52 -0800

    WIP edits to database connection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect, useRef} from "react";
 import AddFeatureForm from "../components/AddFeatureForm";
 import Map from "../components/Map";
 import ListFeatures from "../components/ListFeatures";
-//import {getDatabase, ref, onValue, get, orderByChild, equalTo, query } from 'firebase/database'
+import {getDatabase, ref, onValue, get, orderByChild, equalTo, query } from 'firebase/database'
 import Hero from "../components/Hero";
 import ViewContributeForm from "../components/ViewContributeForm";
 
@@ -10,21 +10,21 @@ export default function Home(props){
 	const [data, setData] = useState()
 	//const [filter, setFilter] = useState('null')
 	const [reset, setReset] = useState(0);
-	const ref = useRef(null);
     const [mapMode, setMapMode] = useState('view');
     const [currentSelection, setCurrentSelection] = useState([[], 'none']);
+	const mapRef = useRef(null);
 
     useEffect(() => {
 		async function contributeMode() {
-			//const db = getDatabase();
-			//const ref = ref(db, "POI");
-			//const orderByType = query(ref, orderByChild('type'), equalTo(filter)) // makes a query to the database to only return values with type=filter
-			//const querySnapshot = await get(orderByType);
-			//setData(querySnapshot)
+			const db = getDatabase();
+			const ref = ref(db, "locations");
+			const orderByType = query(ref, orderByChild('type')) // makes a query to the database
+			const querySnapshot = await get(orderByType);
+			setData(querySnapshot)
 		}
 
 		async function viewMode() {
-
+		
 		}
 
 		if (mapMode === 'view'){ 
@@ -53,18 +53,18 @@ export default function Home(props){
 
 	useEffect(()=>{ //this pulls data from the database on reset
 		async function initialLoad() {
-			//setData(props.locations);
-			//const db = getDatabase();
-			//const racksRef = ref(db, "racks");
-			//const first = await get(racksRef);
-			//setData(first)
+			setData(props.locations);
+			const db = getDatabase();
+			const ref = ref(db, "locations");
+			const first = await get(ref);
+			setData(first)
 		}
 		initialLoad();
 	}, [, reset])
 
 	const scrollToMap = () => {
-		if (ref.current) {
-		  ref.current.scrollIntoView({ behavior: 'smooth' });
+		if (mapRef.current) {
+		  mapRef.current.scrollIntoView({ behavior: 'smooth' });
 		}
 	};
 
